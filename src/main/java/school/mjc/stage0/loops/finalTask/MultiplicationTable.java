159d0e930a836f9cf4bd31960b52453c0b64ffec
unoffending <109032978+unoffending@users.noreply.github.com>
2023-10-20 17:50:47 +0600

    Update MultiplicationTable.java

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+                if(numberTableToPrint < 0) {
+            System.out.println("Please, input a positive integer!");      
+        } else {
+        for(int i = 1; i < 10; i++) {
+            System.out.println(numberTableToPrint + " * " + i + " = " + numberTableToPrint * i);
+        }
+        }
     }
 }
