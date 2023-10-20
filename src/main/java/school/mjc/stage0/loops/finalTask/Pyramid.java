6f93e408eb994c198aa65e331a528dd46d377928
unoffending <109032978+unoffending@users.noreply.github.com>
2023-10-20 17:51:35 +0600

    Update Pyramid.java

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,29 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+  String empty = " ";
+  int lengthEmpty = cathetusLength - 1;
+  int result = 1;
+  int one = 1;
+  int a = 1;
+  int tens = 1;
+  int tens2 = 1;
+  int u;
+  for(int j = 0; j < cathetusLength; j++) {
+      for(int i = lengthEmpty; i > 0; i--) {
+          System.out.print(empty);
+      }
+      for(int i = 0; i < one; i++) {
+          System.out.print(result);
+      }
+      System.out.println();
+      lengthEmpty--;
+      result *= 10;
+      tens2 *= 100;
+      a++;
+      u = (tens2 * a) + a;
+      result += u;
+  }
     }
 
     public static void main(String[] args) {
