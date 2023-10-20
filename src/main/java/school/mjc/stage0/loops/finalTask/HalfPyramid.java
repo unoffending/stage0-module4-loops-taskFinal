961121e4ed32f68ec2d2990a47555417cc70354c
unoffending <109032978+unoffending@users.noreply.github.com>
2023-10-20 17:50:25 +0600

    Update HalfPyramid.java

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,21 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+         String symbol = "*";
+     String empty = " ";
+     int emptyL = cathetusLength - 1;
+     int one = 1;
+     for(int j = 0; j < cathetusLength; j++) {
+      for(int i = emptyL; i > 0; i--) {
+          System.out.print(empty);
+      }
+      for(int i = 0; i < one; i++) {
+          System.out.print(symbol);
+      }
+      System.out.println();
+      one++;
+      emptyL--;
+  }
     }
 
     public static void main(String[] args) {
